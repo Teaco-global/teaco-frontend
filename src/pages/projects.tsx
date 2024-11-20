1d09435e0d2be3f1fa41eb9d87f5689fadc7020a
rjad-dev <adrajesh494@gmail.com>
2024-11-20 19:15:19 +0545

    minor changes

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -87,9 +87,9 @@ const Projects: React.FC = () => {
     return date.toLocaleString(undefined, {
       year: 'numeric',
       month: 'long',
-      day: 'numeric',
-      hour: '2-digit',
-      minute: '2-digit'
+      day: 'numeric'
+      // hour: '2-digit',
+      // minute: '2-digit'
     });
   };
 
