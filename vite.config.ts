c4492c5fe15213dc95c7b311637bd92423752725
rjad-dev <adrajesh494@gmail.com>
2024-12-15 16:30:43 +0545

    revert previous configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import viteTsconfigPaths from 'vite-tsconfig-paths';
 import path from "path";
 
 export default defineConfig(({ mode }) => {
@@ -8,17 +9,14 @@ export default defineConfig(({ mode }) => {
     define: {
       "process.env": env,
     },
-    plugins: [react()],
+    plugins: [react(), viteTsconfigPaths()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
       },
     },
-    server: {
-      fs: {
-        cachedChecks: false,
-        preTransformRequests: false
-      }
-    }
+    build: {
+      chunkSizeWarningLimit: 1600,
+    },
   };
 });
